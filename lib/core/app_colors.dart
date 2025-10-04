8aa071a8f3f30b08c7f94ca5dc801359cad33fcd
Yuto Mabe <yuto96315mabe@gmail.com>
2025-10-04 21:55:59 +0900

    colorの修正

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -4,8 +4,10 @@ class AppColors {
   AppColors._();
 
   // ブランドカラー
-  static const icon = Color(0xFF1C2E45);
+  static const icon = Color(0xFF707070);
+  static const iconBg = Color(0xFFD9D9D9);
   static const primary = Color(0xFF64D9E4);
+  static const textColor = Colors.white;
 
   // ボタン系
   static const buttonPrimary = Color(0xFF0097A7); // 通常ボタン
