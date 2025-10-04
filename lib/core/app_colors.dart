76da59b84a533298d967cbb7831ce867d9a57d97
Yuto Mabe <yuto96315mabe@gmail.com>
2025-10-04 20:35:39 +0900

    カラー追加

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // ブランドカラー
+  static const icon = Color(0xFF1C2E45);
+  static const primary = Color(0xFF64D9E4);
+
+  // ボタン系
+  static const buttonPrimary = Color(0xFF0097A7); // 通常ボタン
+  static const buttonDanger = Color(0xFFE53935); // アラートボタン
+  static const buttonOutline = icon; // アウトラインボタンの線・文字
+}
